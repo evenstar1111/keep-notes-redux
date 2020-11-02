941a4e5d114dafef292da3400e35a2b2fdaaedd4
sk shahil <skshahil35@gmail.com>
2020-11-02 21:38:43 +0530

    changes in navbar

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -5,13 +5,10 @@ import { Link } from "react-router-dom";
 export function NavBar() {
   return (
     <Navbar bg="dark" variant="dark">
-      <Navbar.Toggle aria-controls="app-navbar" />
+      <Link className="navbar-brand" to="/">
+        Todos
+      </Link>
       <Nav className="mr-auto">
-        <Nav.Item>
-          <Link className="nav-link" to="/">
-            Todos
-          </Link>
-        </Nav.Item>
         <Nav.Item>
           <Link className="nav-link" to="/create-todo">
             Create Todo
